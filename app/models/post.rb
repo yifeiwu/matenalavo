c8feb0a8f73c583b60ff0e6ffa0e506c7f362280
yifeiwu <yiferic@gmail.com>
2015-08-10 21:26:50 +1200

    2nd

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
 	acts_as_taggable # Alias for acts_as_taggable_on :tags
-	POST_TYPES=['Job Vacancies', 'Cars for Sale', 'Properties for Sale']
+	POST_TYPES=['Jobs', 'Cars for Sale', 'Properties for Sale', 'Things for Sale']
 end
