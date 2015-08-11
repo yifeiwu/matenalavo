47a4216f79cb250cbf9b578eacba3a24f594a068
yifeiwu <yiferic@gmail.com>
2015-08-11 21:57:34 +1200

    before adding advanced feats

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-
+gem 'thin'
 gem 'acts-as-taggable-on', '~> 3.4'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
 	acts_as_taggable # Alias for acts_as_taggable_on :tags
 	POST_TYPES=['Jobs', 'Cars for Sale', 'Properties for Sale', 'Things for Sale']
+	scope :content_like, -> (content) { where("content ilike ?", content)}
+	scope :category_like, -> (category) { where("category ilike ?", category)}
 end
