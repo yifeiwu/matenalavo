8f93db7f1509250efd08b2470e344c76230d55c6
yifeiwu <yiferic@gmail.com>
2016-02-29 07:42:28 +1000

    moved bootswatch to cdn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,7 +22,7 @@ class PostsController < ApplicationController
         post_date: Post.options_for_post_date
       }
     ) or return
-    @posts = @filterrific.find.page(params[:page]).per(6)
+    @posts = @filterrific.find.page(params[:page]).per(10)
 
     respond_to do |format|
       format.html
